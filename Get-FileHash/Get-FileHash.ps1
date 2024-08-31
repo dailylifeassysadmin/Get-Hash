929135d5428f58993f616c5d4540805fc2af1e04
Ravi Prakash T. <raviindia77@hotmail.com>
2024-08-31 11:43:48 +0530

    Create Get-FileHash.ps1

---
+++ Get-FileHash/Get-FileHash.ps1
@@ -0,0 +1,26 @@
+﻿Write-Host -ForegroundColor White "#########################################################################
+##                                                                     ##
+##                    Author Details:                                  ##
+##                    Copyright © to Mobile Scinece                    ##
+##                    Owner: Ravi Prakash T.                           ##
+##                    Contact: 9*********                              ##
+##                                                                     ##   
+#########################################################################`n"
+
+#Taking file path as input from the user.
+$filePath = Read-Host "File Path with file name and extension"
+
+#Taking Algorithm as input from the user.
+$Algorithm = Read-Host 'What algorithm is needed? Accepted values: SHA1, SHA256, SHA384, SHA512, MD5'
+
+#running Get-FileHash command with if function and printing out the hash value in formatted list.
+if ( $Algorithm -eq "" )
+{
+    Get-FileHash $filePath | Format-List
+}
+else
+{
+    Get-FileHash $filePath -Algorithm $Algorithm | Format-List
+}
+
+Read-Host -Prompt "Press Enter to exit"
